4b8fd978b02ccd3663332757350c073f954c77ce
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-20 19:01:43 +0200

    adjusted hands lenghts

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ function setDate() {
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 24) * 360) + 90;
+  const hoursDegrees = ((hours / 12) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
